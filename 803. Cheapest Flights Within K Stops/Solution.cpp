897e5344174c7bdc56f59b56d9d6ee22aaf66334
BabaYaga <119802864+MrNikoTheNumberFour@users.noreply.github.com>
2024-09-12 11:54:24 +0600

    C++ Solution
            Submission Statistics:
            Question #: 803
            Language: C++
            Runtime: 13 ms
            Runtime percentile: 80.52
            Memory: 16.26 MB
            Memory percentile: 97.05

--- 803. Cheapest Flights Within K Stops/Solution.cpp
+++ 803. Cheapest Flights Within K Stops/Solution.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-      vector<int>dist(n, numeric_limits<int>::max());
-            dist[src]=0;
+//       vector<int>dist(n, numeric_limits<int>::max());
+//             dist[src]=0;
 
-            for(int i=0; i <= k; i++){
+//             for(int i=0; i <= k; i++){
 
-                vector<int>temp(dist);
+//                 vector<int>temp(dist);
 
-                for(auto flight: flights){
+//                 for(auto flight: flights){
 
-                     if(dist[flight[0]] != numeric_limits<int>::max()){
-                          temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
-                     }
+//                      if(dist[flight[0]] != numeric_limits<int>::max()){
+//                           temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
+//                      }
+//                 }
+
+//                 dist = temp;
+//             }
+
+//             return dist[dst] == numeric_limits<int>::max() ? -1 : dist[dst];  
+//     }
+        vector<int> prices(n, numeric_limits<int>::max());
+        prices[src] = 0;
+
+        for (int i = 0; i <= k; ++i) {
+            vector<int> tmpPrices = prices;
+            for (auto& flight : flights) {
+                int s = flight[0], d = flight[1], p = flight[2];
+                if (prices[s] == numeric_limits<int>::max()) {
+                    continue;
                 }
-
-                dist = temp;
+                if (prices[s] + p < tmpPrices[d]) {
+                    tmpPrices[d] = prices[s] + p;
+                }
             }
-
-            return dist[dst] == numeric_limits<int>::max() ? -1 : dist[dst];  
+            prices = tmpPrices;
+        }
+        return prices[dst] == numeric_limits<int>::max() ? -1 : prices[dst];
     }
 };
